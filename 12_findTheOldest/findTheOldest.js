e52f2966ad65ad248c4781c8091e2e0271975f29
MichaelNeely77 <michael@michaelneely.net>
2023-12-22 13:24:22 -0500

    Almost have my mind wrapped around teh problem

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr1) {
+    // Get age of person by subtracting yearOfBirth from yearOfDeath
+    // Return name of oldest person
+return arr1.reduce(())
 
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
